8b5d98f603036664f132780813bd4f4140f38e59
Georgy <louis.georgy@gmail.com>
2018-12-14 14:56:24 -0500

    First update

---
+++ src/main/java/com/dolphinsmemory/exam/DolphinsClientApplication.java
@@ -0,0 +1,12 @@
+package com.dolphinsmemory.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolphinsClientApplication.class, args);
+	}
+}
